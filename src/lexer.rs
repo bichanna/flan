ef3c9effb53501ab3fdf3c7f5fe6ba1c8db44192
bichanna <nobu.bichanna@gmail.com>
2023-01-02 23:45:42 -0500

    Change fn keyword to func

--- src/lexer.rs
+++ src/lexer.rs
@@ -320,7 +320,7 @@ impl<'a> Lexer<'a> {
     /// Returns the TokenType of the keyword if the given &str is a keyword
     fn keyword(value: &str) -> Option<TokenType> {
         match value.to_lowercase().as_str() {
-            "fn" => Some(TokenType::Func),
+            "func" => Some(TokenType::Func),
             "method" => Some(TokenType::Method),
             "class" => Some(TokenType::Class),
             "static" => Some(TokenType::Static),
