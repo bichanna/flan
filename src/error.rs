94f28abf32a1f50b24cc25fc542b62f55a7f94be
bichanna <nobu.bichanna@gmail.com>
2023-01-18 09:56:34 -0500

    Create RuntimeError struct and StackEntry

--- src/error.rs
+++ src/error.rs
@@ -17,7 +17,72 @@ impl ParserError {
     pub fn format(&self, filename: &str) -> String {
         format!(
             "{}:{}:{} error: {}",
-            filename, self.col, self.line, self.msg
+            filename, self.line, self.col, self.msg
+        )
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct StackEntry<'a> {
+    name: Option<&'a str>,
+    line: usize,
+    col: usize,
+}
+
+impl<'a> StackEntry<'a> {
+    pub fn new<'b>(name: Option<&'a str>, line: usize, col: usize) -> Self {
+        Self { name, line, col }
+    }
+
+    pub fn string(&self) -> String {
+        if let Some(name) = self.name {
+            format!("  {}:{}:in func {}", self.line, self.col, name)
+        } else {
+            format!("  {}:{}:in anonymous func", self.line, self.col)
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct RuntimeError<'a> {
+    reason: &'a str,
+    line: usize,
+    col: usize,
+    stack_traces: Vec<StackEntry<'a>>,
+}
+
+impl<'a> RuntimeError<'a> {
+    pub fn new<'b>(
+        reason: &'a str,
+        line: usize,
+        col: usize,
+        stack_traces: Option<Vec<StackEntry<'a>>>,
+    ) -> Self {
+        Self {
+            reason,
+            line,
+            col,
+            stack_traces: if let Some(stack_traces) = stack_traces {
+                stack_traces
+            } else {
+                vec![]
+            },
+        }
+    }
+
+    pub fn string(&self, filename: &str) -> String {
+        format!(
+            "{}\n{}:{}:{} error: {}",
+            self.stack_traces
+                .to_owned()
+                .into_iter()
+                .map(|s| s.string())
+                .collect::<Vec<String>>()
+                .join("\n"),
+            filename,
+            self.line,
+            self.col,
+            self.reason
         )
     }
 }
