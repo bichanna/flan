0b50542be52823a8366da578a95a4bb4c56040b0
bichanna <nobu.bichanna@gmail.com>
2023-01-21 12:47:29 -0500

    Remove interpreter stuff

--- src/interpreter/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod engine;
-pub mod interpreter;
-pub mod scope;
-pub mod value;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 pub mod ast;
 pub mod debug;
 pub mod error;
-pub mod interpreter;
 pub mod lexer;
 pub mod parser;
 pub mod token;
