ccb1f6e186a39d38e4627cf20ad8040c6b825807
bichanna <nobu.bichanna@gmail.com>
2023-08-07 01:35:38 -0400

    Update the description

--- src/cli.rs
+++ src/cli.rs
@@ -7,7 +7,7 @@ static FLAN_VERSION: &str = "0.0.0";
 #[derive(Parser)]
 #[command(author = "Nobuharu Shimazu <nobu.bichanna@gmail.com>")]
 #[command(version = FLAN_VERSION)]
-#[command(about = "A simple, expression oriented programming language", long_about = None)]
+#[command(about = "A simple, intuitive, expression-oriented programming language", long_about = None)]
 pub struct Cli {
     /// Input file
     #[arg(value_name = "INPUT")]
